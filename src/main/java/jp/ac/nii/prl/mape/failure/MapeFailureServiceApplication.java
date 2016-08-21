88cc93f6de82ad94c44ef6a520fab90bafadcb03
Lionel Montrieux <lionel@nii.ac.jp>
2016-08-21 10:14:11 +0900

    Initial project import

---
+++ src/main/java/jp/ac/nii/prl/mape/failure/MapeFailureServiceApplication.java
@@ -0,0 +1,12 @@
+package jp.ac.nii.prl.mape.failure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeFailureServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeFailureServiceApplication.class, args);
+	}
+}
